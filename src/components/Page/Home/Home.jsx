f95ac46066f4cbf6971b2894d774c849de4c2f0e
Kaleb <kalebmulugeta07@gmail.com>
2025-11-29 02:34:19 -0600

    home

--- src/components/Page/Home/Home.jsx
+++ src/components/Page/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+import Banner from '../../components/Banner/Banner'
+import RowList from "../../components/Rows/RowList/RowList";
 
-function Home() {
+const Home = () => {
   return (
     <div>
-      
+      <Header/>
     </div>
   )
 }
