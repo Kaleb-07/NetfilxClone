cedefc30483d366f87810b2a8f54ab9d9485d98d
Kaleb <kalebmulugeta07@gmail.com>
2025-11-29 02:02:51 -0600

    add code in header files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import ('./Header.css');
+import Netflix_logo from "../../assets/images/Netflix_logo.svg";
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+function Header() {
+  return (
+    <div className='header_outer_container'>
+      <div className='header_container'>
+        <div className='header-left'>
+          <ul>
+            <li><img src={Netflix_logo} alt="netflix logo"/></li>
+            <li>Home</li>
+            <li>TvShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+        <div className='header_right'>
+          <ul>
+            <li><SearchIcon/></li>
+            <li><NotificationsIcon/></li>
+            <li><AccountBoxIcon/></li>
+            <li><ArrowDropDownIcon/></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
