67f0ab943491ef7212c6496583e81de55ec41132
Kaleb <kalebmulugeta07@gmail.com>
2025-12-02 14:09:38 -0600

    updating the rowlist code

--- src/components/Rows/RowList/RowList.jsx
+++ src/components/Rows/RowList/RowList.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import Row from "../Row/Row";
+import requests from "../../../utils/requests";
 
 function RowList() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLarge
+      />
+
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </>
+  );
 }
 
-export default RowList
+export default RowList;
