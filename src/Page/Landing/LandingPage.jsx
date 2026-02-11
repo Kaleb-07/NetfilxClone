c1eec526ed1164ab956dc92d64ee9042fa9d2319
Kaleb <kalebmulugeta07@gmail.com>
2026-02-11 09:45:05 -0600

    updting th hero image

--- src/Page/Landing/LandingPage.jsx
+++ src/Page/Landing/LandingPage.jsx
@@ -10,6 +10,9 @@ const LandingPage = () => {
     return (
         <div className="landing-page">
             <LandingHero />
+            <div className="curve-container">
+                <div className="default-ltr-iqcdef-cache-1f97ztc"></div>
+            </div>
             <TrendingSection title="Trending Now" />
             <ReasonsToJoin />
             <FAQSection />
