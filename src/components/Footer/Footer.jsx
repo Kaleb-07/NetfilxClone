5d7d6aa2e749b1c057ea4ea4c307f53cadff2bb2
Kaleb <kalebmulugeta07@gmail.com>
2026-02-12 11:34:42 -0600

    updating the footer background

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,54 +16,56 @@ function Footer() {
 
   return (
     <footer className="footer_container">
-      <div className="footer_social">
-        <a href="https://www.facebook.com/netflix" target="_blank" rel="noreferrer"><FacebookIcon /></a>
-        <a href="https://www.instagram.com/netflix" target="_blank" rel="noreferrer"><InstagramIcon /></a>
-        <a href="https://twitter.com/netflix" target="_blank" rel="noreferrer"><TwitterIcon /></a>
-        <a href="https://www.youtube.com/user/NewOnNetflix" target="_blank" rel="noreferrer"><YouTubeIcon /></a>
-      </div>
+      <div className="footer_content">
+        <div className="footer_social">
+          <a href="https://www.facebook.com/netflix" target="_blank" rel="noreferrer"><FacebookIcon /></a>
+          <a href="https://www.instagram.com/netflix" target="_blank" rel="noreferrer"><InstagramIcon /></a>
+          <a href="https://twitter.com/netflix" target="_blank" rel="noreferrer"><TwitterIcon /></a>
+          <a href="https://www.youtube.com/user/NewOnNetflix" target="_blank" rel="noreferrer"><YouTubeIcon /></a>
+        </div>
 
-      <div className="footer_links">
-        <div className="footer_column">
-          <Link to="#">{t('footer.audioDescription')}</Link>
-          <Link to="#">{t('footer.investorRelations')}</Link>
-          <Link to="#">{t('footer.legalNotices')}</Link>
-          <Link to="#" onClick={(e) => { e.preventDefault(); setShowServiceCode(!showServiceCode); }}>
-            {t('footer.serviceCode')}
-          </Link>
+        <div className="footer_links">
+          <div className="footer_column">
+            <Link to="#">{t('footer.audioDescription')}</Link>
+            <Link to="#">{t('footer.investorRelations')}</Link>
+            <Link to="#">{t('footer.legalNotices')}</Link>
+            <Link to="#" onClick={(e) => { e.preventDefault(); setShowServiceCode(!showServiceCode); }}>
+              {t('footer.serviceCode')}
+            </Link>
+          </div>
+          <div className="footer_column">
+            <Link to="/help">{t('footer.helpCenter')}</Link>
+            <Link to="#">{t('footer.jobs')}</Link>
+            <Link to="#">{t('footer.cookiePreferences')}</Link>
+          </div>
+          <div className="footer_column">
+            <Link to="#">{t('footer.giftCards')}</Link>
+            <Link to="#">{t('footer.termsOfUse')}</Link>
+            <Link to="#">{t('footer.corporateInformation')}</Link>
+          </div>
+          <div className="footer_column">
+            <Link to="#">{t('footer.mediaCenter')}</Link>
+            <Link to="#">{t('footer.privacy')}</Link>
+            <Link to="#">{t('footer.contactUs')}</Link>
+          </div>
         </div>
-        <div className="footer_column">
-          <Link to="/help">{t('footer.helpCenter')}</Link>
-          <Link to="#">{t('footer.jobs')}</Link>
-          <Link to="#">{t('footer.cookiePreferences')}</Link>
-        </div>
-        <div className="footer_column">
-          <Link to="#">{t('footer.giftCards')}</Link>
-          <Link to="#">{t('footer.termsOfUse')}</Link>
-          <Link to="#">{t('footer.corporateInformation')}</Link>
-        </div>
-        <div className="footer_column">
-          <Link to="#">{t('footer.mediaCenter')}</Link>
-          <Link to="#">{t('footer.privacy')}</Link>
-          <Link to="#">{t('footer.contactUs')}</Link>
-        </div>
-      </div>
 
-      <div className="footer_service">
-        <div className="lang-selector">
-          <LanguageIcon className="globe-icon" />
-          <select value={language} onChange={(e) => changeLanguage(e.target.value)}>
-            <option value="en">English</option>
-            <option value="es">Español</option>
-          </select>
+        <div className="footer_service">
+          <div className="lang-selector">
+            <LanguageIcon className="globe-icon" />
+            <select value={language} onChange={(e) => changeLanguage(e.target.value)}>
+              <option value="en">English</option>
+              <option value="es">Español</option>
+            </select>
+          </div>
+          <button className="service_code_btn" onClick={() => setShowServiceCode(!showServiceCode)}>
+            {showServiceCode ? serviceCode : t('footer.serviceCode')}
+          </button>
         </div>
-        <button className="service_code_btn" onClick={() => setShowServiceCode(!showServiceCode)}>
-          {showServiceCode ? serviceCode : t('footer.serviceCode')}
-        </button>
-      </div>
 
-      <div className="footer_copy">
-        {t('footer.copyright')}
+        <div className="footer_copy">
+          {t('footer.copyright')}
+        </div>
       </div>
     </footer>
   );
