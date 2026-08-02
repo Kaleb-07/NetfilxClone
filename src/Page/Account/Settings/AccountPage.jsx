389379abfddd11bfd86990cca6f14872fd5a4184
Kaleb Mulugeta Estifanos <kalebmulugeta07@gmail.com>
2026-08-02 13:52:25 -0500

    Update AccountPage.jsx

--- src/Page/Account/Settings/AccountPage.jsx
+++ src/Page/Account/Settings/AccountPage.jsx
@@ -50,7 +50,7 @@ const AccountPage = () => {
 
             openEditModal(target, currentValue);
 
-            // Clear state so it doesn't need reopen on refresh
+            // Clear state so it doesn't need reopen on refresh to see the
             window.history.replaceState({}, document.title);
         }
     }, [location.state, currentUser.email, currentUser.phone]);
