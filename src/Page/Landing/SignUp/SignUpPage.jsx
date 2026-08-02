a110b42986a0ce47735544195129eb4907471a5d
Kaleb Mulugeta Estifanos <kalebmulugeta07@gmail.com>
2026-08-02 13:54:34 -0500

    Update SignUpPage.jsx

--- src/Page/Landing/SignUp/SignUpPage.jsx
+++ src/Page/Landing/SignUp/SignUpPage.jsx
@@ -30,7 +30,7 @@ const SignUpPage = () => {
             localStorage.setItem('registeredEmails', JSON.stringify(registeredEmails));
         }
 
-        // Store full user 
+        // Store full users
         registeredUsers[formData.email.toLowerCase()] = {
             username: formData.username,
             password: formData.password
