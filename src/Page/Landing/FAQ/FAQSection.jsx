e000f63d1c9456b6c68c90535784a52d6474ddc1
Kaleb Mulugeta Estifanos <kalebmulugeta07@gmail.com>
2026-08-30 14:00:06 -0500

    Update FAQSection.jsx

--- src/Page/Landing/FAQ/FAQSection.jsx
+++ src/Page/Landing/FAQ/FAQSection.jsx
@@ -13,7 +13,7 @@ const FAQSection = () => {
             answer: "Netflix is a streaming services that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.\n\nYou can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!"
         },
         {
-            question: "How much does Netflix cost?",
+            question: "How much does Netflix costs?",
             answer: "Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from US$6.99 to US$22.99 a month. No extra costs, no contracts."
         },
         {
